0fab70248c31301d43a8a7db96f7b9f078f731eb
Whyyu98 <73743811+Whyyu98@users.noreply.github.com>
2024-11-01 11:16:50 +0800

    Delete pleiotropic loci directory

--- pleiotropic loci/clump.sh
+++
@@ -1,9 +0,0 @@
-﻿#!/bin/bash
-
-folder_path="/mnt/e/GXN/MTAG_CPASSOC/MTAG_CPASSOC" 
-
-for txt_file in "$folder_path"/*.txt; do
-  file_prefix=$(basename "$txt_file" .txt)
-  
-  ./plink --bfile ./g1000_eur/g1000_eur --clump-p1 1 --clump-r2 0.2 --clump-kb 500 --clump "$txt_file" --clump-snp-field SNP --clump-field P.MTAG --out ./SNV/"$file_prefix"
-done
